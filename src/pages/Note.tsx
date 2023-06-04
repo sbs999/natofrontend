916628ca56eb61703ace25e33a275bb8383f7308
saba pachulia <saba.pachulia@makingscience.com>
2023-06-04 19:41:11 +0400

    fix small typos

--- src/pages/Note.tsx
+++ src/pages/Note.tsx
@@ -40,37 +40,21 @@ function Note() {
 
   return (
     <div>
-      <button
-        onClick={() => navigate("/")}
-        type="submit"
-        className="border-[1px] bg-[#3498db] p-[10px] rounded-[12px] text-white mt-[10px] ml-[10px]"
-      >
-        უკან გასვლა
-      </button>
+      <div className="flex justify-between">
+        <button
+          onClick={() => navigate("/")}
+          type="submit"
+          className="border-[1px] bg-[#3498db] p-[10px] rounded-[12px] text-white mt-[10px] ml-[10px]"
+        >
+          უკან გასვლა
+        </button>
 
-      <p className="text-center text-[24px] my-[10px] font-bold text-green-500">
-        ჩანაწერები
-      </p>
-
-      <div className="w-[95%] h-[75vh] border border-black rounded-[10px]  mx-[2.5%]">
-        {status === "edit" ? (
-          <textarea
-            value={content.newNote}
-            onChange={(e) =>
-              setContent((note) => ({ ...note, newNote: e.target.value }))
-            }
-            className="w-[100%] max-h-[75vh] min-h-[75vh] text-[21px] p-2"
-          />
-        ) : (
-          <textarea
-            readOnly
-            value={content.saveNote}
-            className="w-[100%] h-[75vh] overflow-hidden overflow-y-scroll text-[21px] p-2"
-          />
-        )}
+        <p className="text-center text-[20px] my-[10px] font-bold text-green-500 mr-[2.5%]">
+          ჩანაწერები
+        </p>
       </div>
 
-      <div className="w-[95%] mx-auto flex justify-end mt-[10px]">
+      <div className="w-[95%] mx-auto flex justify-end mt-[20px] mb-[15px]">
         {status === "readOnly" ? (
           <button
             onClick={() => setStatus("edit")}
@@ -100,6 +84,24 @@ function Note() {
               უარყოფა
             </button>
           </div>
+        )}
+      </div>
+
+      <div className="w-[95%] h-[75vh] border border-black rounded-[10px]  mx-[2.5%]">
+        {status === "edit" ? (
+          <textarea
+            value={content.newNote}
+            onChange={(e) =>
+              setContent((note) => ({ ...note, newNote: e.target.value }))
+            }
+            className="w-[100%] max-h-[75vh] min-h-[75vh] text-[21px] p-2"
+          />
+        ) : (
+          <textarea
+            readOnly
+            value={content.saveNote}
+            className="w-[100%] h-[75vh] overflow-hidden overflow-y-scroll text-[21px] p-2"
+          />
         )}
       </div>
     </div>
